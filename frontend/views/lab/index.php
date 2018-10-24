ba8f0b65896798eaf0f82323c389c238675384cd
shavelsy <ruraider.cuber@gmail.com>
2018-10-25 02:48:06 +0300

    grid

--- frontend/views/lab/index.php
+++ frontend/views/lab/index.php
@@ -1,28 +1,53 @@
-<div id="placeholder" style="width:600px;height:300px;"></div>
+<canvas id="canvas" width="1000" height="1000"></canvas>
 
 <script language="javascript" type="text/javascript">
-    var all_data = [
-        {data: [["2010/10/01", 0], ["2010/10/5", 1], ["2010/10/10", 7], ["2010/10/15", 8]]},
-        {data: [["2010/10/01", 13], ["2010/10/5", 23], ["2010/10/10", 32], ["2010/10/15", 33]]}
-    ];
-    // преобразуем даты в UTC
-    for (var j = 0; j < all_data.length; ++j) {
-        for (var i = 0; i < all_data[j].data.length; ++i)
-            all_data[j].data[i][0] = Date.parse(all_data[j].data[i][0]);
+    var width = 700;
+    var height = 400;
+    var border = 2;
+    var gridSpacingMain = 100;
+    var gridSpacingSecond = 20;
+
+
+    var canvas = document.getElementById('canvas');
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+
+        // основной прямоугольник
+        ctx.fillStyle = 'rgb(93, 177, 162)';
+        ctx.fillRect(border, border, width, height);
+
+        // внешняя рамка
+        ctx.strokeStyle = 'rgb(223, 223, 223)';
+        ctx.strokeRect(1, 1, width + border, height + border);
+
+        // вспомогательная сетка
+        ctx.strokeStyle = 'rgb(255, 86, 76)';
+        drawVerticalLines(ctx, gridSpacingSecond);
+        drawHorizontalLines(ctx, gridSpacingSecond);
+        
+        // основная сетка
+        ctx.strokeStyle = 'rgb(24, 86, 76)';
+        drawVerticalLines(ctx, gridSpacingMain);
+        drawHorizontalLines(ctx, gridSpacingMain);
     }
 
-    var plot_conf = {
-        series: {
-            lines: {
-                show: true,
-                lineWidth: 2
-            }
-        },
-        xaxis: {
-            mode: "time",
-            timeformat: "%y/%m/%d",
+    function drawVerticalLines(ctx, gridSpacing) {
+        var i = gridSpacing + border;
+        while (i < width + border) {
+            ctx.moveTo(i, border);
+            ctx.lineTo(i, height + border);
+            ctx.stroke();
+            i = i + gridSpacing;
         }
-    };
+    }
 
-    $.plot($("#placeholder"), all_data, plot_conf);
+    function drawHorizontalLines(ctx, gridSpacing) {
+        var i = gridSpacing + border;
+        while (i < height + border) {
+            ctx.moveTo(border, i);
+            ctx.lineTo(width + border, i);
+            ctx.stroke();
+            i = i + gridSpacing;
+        }
+    }
 </script>
