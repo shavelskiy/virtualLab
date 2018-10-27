4ad1185d887532ce03a752877db0fa3bc892aa9d
shavelsy <ruraider.cuber@gmail.com>
2018-10-28 01:12:37 +0300

    add students variant

--- backend/models/Student.php
+++ backend/models/Student.php
@@ -13,6 +13,7 @@ use common\models\User;
  * @property string $name
  * @property string $last_name
  * @property string $middle_name
+ * @property integer $variant
  * @property int $group_id
  *
  * @property Groups $group
@@ -35,8 +36,8 @@ class Student extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['user_id', 'name', 'last_name', 'group_id'], 'required'],
-            [['user_id', 'group_id'], 'integer'],
+            [['user_id', 'name', 'last_name', 'variant', 'group_id'], 'required'],
+            [['user_id', 'variant', 'group_id'], 'integer'],
             [['name', 'last_name', 'middle_name'], 'string', 'max' => 255],
             [['group_id'], 'exist', 'skipOnError' => true, 'targetClass' => Groups::className(), 'targetAttribute' => ['group_id' => 'id']],
             [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
@@ -54,6 +55,7 @@ class Student extends \yii\db\ActiveRecord
             'name' => 'Имя',
             'last_name' => 'Фамилия',
             'middle_name' => 'Отчество',
+            'variant' => 'Вариант',
             'group_id' => 'Group ID',
         ];
     }
