66989362fec84b20ddd3c8919f12b688f45fdd3a
shavelsy <ruraider.cuber@gmail.com>
2019-01-05 23:27:05 +0300

    fix merge conflicts

--- htdocs/backend/views/site/login.php
+++ htdocs/backend/views/site/login.php
@@ -2,32 +2,36 @@
 
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
+
 /* @var $model \common\models\LoginForm */
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
-$this->title = 'Login';
+$this->title = 'Авторизация';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to login:</p>
+    <?php if ($errorAccess): ?>
+        <div class="alert alert-danger">У вас нет доступа к админке</div>
+    <?php endif; ?>
+    <p>Пожалуйста, заполните следующие поля:</p>
 
     <div class="row">
         <div class="col-lg-5">
             <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+            <?= $form->field($model, 'username')->textInput(['autofocus' => true])->label('Логин') ?>
 
-                <?= $form->field($model, 'password')->passwordInput() ?>
+            <?= $form->field($model, 'password')->passwordInput()->label('Пароль') ?>
 
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+            <?= $form->field($model, 'rememberMe')->checkbox()->label('Запомнить') ?>
 
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
+            <div class="form-group">
+                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+            </div>
 
             <?php ActiveForm::end(); ?>
         </div>
--- htdocs/frontend/views/site/error.php
+++ htdocs/frontend/views/site/error.php
@@ -1,12 +1,9 @@
 <?php
-
 /* @var $this yii\web\View */
 /* @var $name string */
 /* @var $message string */
 /* @var $exception Exception */
-
 use yii\helpers\Html;
-
 $this->title = $name;
 ?>
 <div class="site-error">
