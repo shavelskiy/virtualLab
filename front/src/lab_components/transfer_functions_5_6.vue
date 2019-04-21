3ecb121c33ee74e00a1e87325ee2743804e5f62f
Vladislav Shavelsky <v.shavelsky@gmail.com>
2019-04-21 16:29:24 +0300

    pfr fuction

--- front/src/lab_components/transfer_functions_5_6.vue
+++ front/src/lab_components/transfer_functions_5_6.vue
@@ -66,21 +66,23 @@
             </tr>
             </tbody>
         </table>
-        <div class="col-6 ml-5">
-            <div class="col-8 ml-4">
-                <label>Функция для АЧХ</label>
-                <input type="text" class="form-control" v-model="afrFunction">
-                <button type="button" class="btn btn-primary mt-4" v-on:click="drawAfr">Построить</button>
+        <div class="row">
+            <div class="col-5 ml-5">
+                <div class="col-8 ml-4">
+                    <label>Функция для АЧХ</label>
+                    <input type="text" class="form-control" v-model="afrFunction">
+                    <button type="button" class="btn btn-primary mt-4" v-on:click="drawAfr">Построить</button>
+                </div>
+                <canvas v-bind:id="afrId"  width="400" height="400" class="mt-3"></canvas>
             </div>
-            <canvas id="afr" width="700" height="400" class="mt-3"></canvas>
-        </div>
-        <div class="col-6 ml-5">
-            <div class="col-8 ml-4">
-                <label>Функция для Фчх</label>
-                <input type="text" class="form-control" v-model="pfrFunction">
-                <button type="button" class="btn btn-primary mt-4" v-on:click="drawPfr">Построить</button>
+            <div class="col-5 ml-5">
+                <div class="col-8 ml-4">
+                    <label>Функция для Фчх</label>
+                    <input type="text" class="form-control" v-model="pfrFunction">
+                    <button type="button" class="btn btn-primary mt-4" v-on:click="drawPfr">Построить</button>
+                </div>
+                <canvas v-bind:id="pfrId" width="400" height="400" class="mt-3"></canvas>
             </div>
-            <canvas id="pfr" width="700" height="400" class="mt-3"></canvas>
         </div>
     </div>
 </template>
@@ -94,6 +96,10 @@
                 freq: [],
                 amplitude: [],
                 phase: [],
+
+                afrId: null,
+                pfrId: null,
+
                 afrCanvas: null,
                 afrContext: null,
                 afrFunction: null,
@@ -105,98 +111,66 @@
         },
 
         methods: {
-            /**
-             * Сторим ачх
-             */
             drawAfr: function () {
-                this.drawGrid(this.afrCanvas, this.afrContext, "f, кГц", "100", "A", "1");
-                var f, a, func;
-                this.afrContext.lineWidth = 2;
-                this.afrContext.strokeStyle = 'rgb(255, 0, 0)';
-
-                this.afrContext.beginPath();
-                this.afrContext.moveTo(50, this.afrCanvas.height - 50);
-
-                func = this.afrFunction ? this.afrFunction : '0';
-
-                for (f = 0; f <= 110; f++) {
-                    a = eval(func.replace(/sqrt/g, 'Math.sqrt'));
-                    this.afrContext.lineTo(
-                        50 + f * 5,
-                        this.afrCanvas.height - 50 - a * 250
-                    );
-                }
-
-                this.afrContext.stroke();
-                this.afrContext.closePath();
-
-                this.afrContext.strokeStyle = 'rgb(36, 93, 210)';
-
-                this.afrContext.beginPath();
-                this.afrContext.moveTo(50, this.afrCanvas.height - 50);
-
-                for (var i = 0; i < 11; i++) {
-                    this.afrContext.lineTo(
-                        this.freq[i] * 5 + 50,
-                        this.afrCanvas.height - this.amplitude[i] * 250 - 50
-                    );
-                }
-
-                this.afrContext.stroke();
-                this.afrContext.closePath();
+                this.drawGrid(this.afrCanvas, this.afrContext);
+                this.drawArrows(this.afrCanvas, this.afrContext, 50);
+                this.drawGraph(this.afrCanvas, this.afrContext, this.afrFunction ? this.afrFunction : '0', 50, 250, this.amplitude);
             },
 
-            /**
-             * Сторим фчх
-             */
             drawPfr: function () {
-                this.drawGrid(this.pfrCanvas, this.pfrContext, "f, кГц", "100", "A", "1");
+                this.drawGrid(this.pfrCanvas, this.pfrContext);
+                this.drawArrows(this.pfrCanvas, this.pfrContext, 200);
+                this.drawGraph(this.pfrCanvas, this.pfrContext, this.pfrFunction ? this.pfrFunction : '0', 200, 200 / Math.PI, this.phase);
+            },
 
-                var f, p, func, x, y, isStart;
+            drawGraph(canvas, context, func, offset, k, tableData) {
+                var f, isStart, x, y;
+
+                func = func.replace(/sqrt/g, 'Math.sqrt').replace(/atan/g, 'Math.atan').replace(/pi/g, 'Math.PI');
+
                 isStart = true;
-                func = this.pfrFunction ? this.pfrFunction : '0';
 
-                this.pfrContext.lineWidth = 2;
-                this.pfrContext.strokeStyle = 'rgb(255, 0, 0)';
-
-                this.pfrContext.beginPath();
+                context.lineWidth = 2;
+                context.strokeStyle = 'rgb(255, 0, 0)';
+                context.beginPath();
 
                 for (f = 0; f <= 110; f++) {
-                    console.log(Math.atan(0))
-                    p = eval(func.replace(/atan/g, 'Math.atan').replace(/PI/g, 'Math.PI'));
-                    x = 50 + f * 5;
-                    y = this.pfrCanvas.height - 200 - p * 200 / Math.PI;
+                    x = 50 + f * 2;
+                    y = canvas.height - offset - eval(func) * k;
                     if (isStart) {
-                        this.pfrContext.moveTo(x, y);
+                        context.moveTo(x, y);
                         isStart = false;
                     } else {
-                        this.pfrContext.lineTo(x, y);
+                        context.lineTo(x, y);
+                    }
+                }
+                context.stroke();
+                context.closePath();
+
+                isStart = true;
+
+                context.strokeStyle = 'rgb(36, 93, 210)';
+                context.beginPath();
+
+                for (var i = 0; i < 11; i++) {
+                    x = this.freq[i] * 2 + 50;
+                    y = canvas.height - offset - tableData[i] * k;
+                    if (isStart) {
+                        context.moveTo(x, y);
+                        isStart = false;
+                    } else {
+                        context.lineTo(x, y);
                     }
                 }
 
-                this.pfrContext.stroke();
-                this.pfrContext.closePath();
-
-                // this.pfrContext.strokeStyle = 'rgb(36, 93, 210)';
-                //
-                // this.pfrContext.beginPath();
-                // this.pfrContext.moveTo(50, this.pfrCanvas.height - 50);
-                //
-                // for (var i = 0; i < 11; i++) {
-                //     this.pfrContext.lineTo(
-                //         this.freq[i] * 5 + 50,
-                //         this.pfrCanvas.height - 200 - this.phase[i] * 200 / Math.PI
-                //     );
-                // }
-                //
-                // this.pfrContext.stroke();
-                // this.pfrContext.closePath();
+                context.stroke();
+                context.closePath();
             },
 
             /**
              * Русиуем разметку
              */
-            drawGrid: function (canvas, context, xText, xValue, yText, yValue) {
+            drawGrid: function (canvas, context) {
                 context.clearRect(0, 0, canvas.width, canvas.height);
 
                 // основной прямоугольник
@@ -221,63 +195,6 @@
                 context.stroke();
                 context.closePath();
 
-                //русуем оси
-                context.strokeStyle = "rgb(0, 0, 0)";
-                context.fillStyle = "rgb(0, 0, 0)";
-                context.lineWidth = 2.0;
-
-                context.beginPath();
-                context.moveTo(50, canvas.height - 50);
-                context.lineTo(canvas.width - 70, canvas.height - 50);
-                context.stroke();
-
-                context.moveTo(50, canvas.height - 50);
-                context.lineTo(50, 70);
-                context.stroke();
-
-                context.moveTo(canvas.width - 50, canvas.height - 50);
-                context.lineTo(canvas.width - 70, canvas.height - 55);
-                context.lineTo(canvas.width - 70, canvas.height - 45);
-                context.fill();
-
-                context.moveTo(50, 50);
-                context.lineTo(45, 70);
-                context.lineTo(55, 70);
-                context.fill();
-
-                var i = 100;
-                while (i < canvas.width - 50) {
-                    context.moveTo(i, canvas.height - 55);
-                    context.lineTo(i, canvas.height - 45);
-                    i = i + 50;
-                }
-                context.stroke();
-
-                var i = 100;
-                while (i < canvas.height - 50) {
-                    context.moveTo(45, i);
-                    context.lineTo(55, i);
-                    i = i + 50;
-                }
-                context.stroke();
-                context.closePath();
-
-                // подписываем оси
-                context.font = "bold 16px sans-serif";
-                context.fillText("0", 38, canvas.height - 34);
-                context.fillText(yText, 20, 65);
-                context.fillText(
-                    xText,
-                    canvas.width - 80,
-                    canvas.height - 30
-                );
-                context.fillText(
-                    xValue,
-                    canvas.width - 150,
-                    canvas.height - 28
-                );
-                context.fillText(yValue, 20, 100);
-
                 // внешняя рамка
                 context.strokeStyle = "rgb(0, 0, 0)";
                 context.lineWidth = 1.0;
@@ -301,19 +218,83 @@
                     }
                 }
             },
+
+            drawArrows(canvas, context, offset) {
+                context.strokeStyle = "rgb(0, 0, 0)";
+                context.fillStyle = "rgb(0, 0, 0)";
+                context.lineWidth = 2.0;
+
+                context.beginPath();
+                context.moveTo(50, canvas.height - offset);
+                context.lineTo(canvas.width - 70, canvas.height - offset);
+                context.stroke();
+
+                context.moveTo(50, canvas.height - 50);
+                context.lineTo(50, 70);
+                context.stroke();
+
+                context.moveTo(canvas.width - 50, canvas.height - offset);
+                context.lineTo(canvas.width - 70, canvas.height - offset - 5);
+                context.lineTo(canvas.width - 70, canvas.height - offset + 5);
+                context.fill();
+
+                context.moveTo(50, 50);
+                context.lineTo(45, 70);
+                context.lineTo(55, 70);
+                context.fill();
+
+                var i = 100;
+                while (i < canvas.width - 50) {
+                    context.moveTo(i, canvas.height - offset + 5);
+                    context.lineTo(i, canvas.height - offset - 5);
+                    i = i + 50;
+                }
+                context.stroke();
+
+                var i = 100;
+                while (i < canvas.height - 50) {
+                    context.moveTo(45, i);
+                    context.lineTo(55, i);
+                    i = i + 50;
+                }
+                context.stroke();
+                context.closePath();
+
+                // подписываем оси
+                context.font = "bold 16px sans-serif";
+                context.fillText("0", 38, canvas.height - offset + 16);
+                context.fillText("f, кГц", canvas.width - 80, canvas.height - offset + 20);
+                context.fillText("100", canvas.width - 150, canvas.height - offset + 22);
+
+                if (offset > 100) {
+                    context.fillText("P", 20, 65);
+                    context.fillText("pi/2", 13, 100);
+                    context.fillText("-pi/2", 10, 300);
+                } else {
+                    context.fillText("A", 20, 65);
+                    context.fillText("1", 20, 100);
+                }
+            }
+        },
+
+        created() {
+          this.afrId = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+          this.pfrId = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
         },
 
         mounted() {
-            this.afrCanvas = document.getElementById("afr");
+            this.afrCanvas = document.getElementById(this.afrId);
             if (this.afrCanvas.getContext) {
                 this.afrContext = this.afrCanvas.getContext("2d");
-                this.drawGrid(this.afrCanvas, this.afrContext, "f, кГц", "100", "A", "1");
+                this.drawGrid(this.afrCanvas, this.afrContext);
+                this.drawArrows(this.afrCanvas, this.afrContext, 50);
             }
 
-            this.pfrCanvas = document.getElementById("pfr");
+            this.pfrCanvas = document.getElementById(this.pfrId);
             if (this.pfrCanvas.getContext) {
                 this.pfrContext = this.pfrCanvas.getContext("2d");
-                this.drawGrid(this.pfrCanvas, this.pfrContext, "f, кГц", "100", "A", "1");
+                this.drawGrid(this.pfrCanvas, this.pfrContext);
+                this.drawArrows(this.pfrCanvas, this.pfrContext, 200);
             }
         }
     }
