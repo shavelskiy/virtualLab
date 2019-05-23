689d09cf0722384215fdb1f0a72b81a70048abf5
Vladislav Shavelsky <v.shavelsky@ailove.ru>
2019-05-23 16:19:40 +0300

    preloader after pdf save

--- htdocs/frontend/controllers/LabController.php
+++ htdocs/frontend/controllers/LabController.php
@@ -6,15 +6,19 @@ use common\models\Component;
 use common\models\Lab;
 use common\models\LabItems;
 use common\models\Student;
+use common\models\User;
+use Jurosh\PDFMerge\PDFMerger;
 use Yii;
 use yii\helpers\ArrayHelper;
+use yii\helpers\Json;
 use yii\web\Controller;
 use yii\filters\AccessControl;
 use yii\web\Response;
-use common\models\User;
 
 class LabController extends Controller
 {
+    public $enableCsrfValidation = false;
+
     /**
      * {@inheritdoc}
      */
@@ -25,7 +29,7 @@ class LabController extends Controller
                 'class' => AccessControl::className(),
                 'rules' => [
                     [
-                        'actions' => ['index', 'lab', 'title-info'],
+                        'actions' => ['index', 'lab', 'title-info', 'result'],
                         'allow' => true,
                         'roles' => ['student'],
                     ],
@@ -145,5 +149,54 @@ class LabController extends Controller
         $result = ArrayHelper::map($components, 'id', 'name');
 
         return $result;
+    }
+
+    public function actionResult()
+    {
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        Yii::$app->response->statusCode = 400;
+
+        $session = Yii::$app->session;
+        $request = Json::decode(Yii::$app->getRequest()->getRawBody());
+
+        if ($session->has('lab_number')) {
+            $lab = Lab::findOne($session->get('lab_number'));
+
+            if (isset($request['taskPdf']) && isset($request['titlePdf'])) {
+                $user = User::find()->andWhere(['id' => Yii::$app->user->id])->one();
+                $fileName = $user->username . '_' . $lab->id . '_' . time();
+
+                $taskPdfBase64 = htmlspecialchars($request['taskPdf']);
+                $titlePdfBase64 = htmlspecialchars($request['titlePdf']);
+
+                $titlePdf = base64_decode($titlePdfBase64);
+                $taskPdf = base64_decode($taskPdfBase64);
+
+                $titlePath = __DIR__ . "/tmp/title_$fileName.pdf";
+                $taskPath = __DIR__ . "/tmp/task_$fileName.pdf";
+
+                $resultPath = $_SERVER['DOCUMENT_ROOT'] . "../../../data/results/$fileName.pdf";
+
+                file_put_contents($titlePath, $titlePdf);
+                file_put_contents($taskPath, $taskPdf);
+
+                $pdf = new PDFMerger();
+
+                $pdf->addPDF($titlePath, 'all', 'vertical')->addPDF($taskPath, 'all', 'vertical');
+                $pdf->merge('file', $resultPath);
+
+                unlink($titlePath);
+                unlink($taskPath);
+
+                Yii::$app->response->statusCode = 200;
+                return [
+                    'date' => date('d.m.Y'),
+                    'file_path' => "/data/results/$fileName.pdf",
+                    'attempt'=> 2
+                ];
+            }
+        }
+
+        return null;
     }
 }
